aff8e7ebdf69e3331fa68c6c5052813a88a4cc64
baongocx1x2 <164115778+baongocx1x2@users.noreply.github.com>
2025-12-04 20:26:07 +0700

    feat: add admin page

--- ChronosX_CK_LTW/src/main/java/controller/AdminServlet.java
+++ ChronosX_CK_LTW/src/main/java/controller/AdminServlet.java
@@ -1,5 +1,46 @@
 package controller;
 
-public class AdminServlet {
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.util.List;
+import dao.ProductDAO;
+import model.Product;
 
+public class AdminServlet extends HttpServlet {
+    private ProductDAO dao = new ProductDAO();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Product> products = dao.getAllProducts();
+        request.setAttribute("products", products);
+        RequestDispatcher rd = request.getRequestDispatcher("/admin.jsp");
+        rd.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if ("add".equals(action)) {
+            String name = request.getParameter("name");
+            String brand = request.getParameter("brand");
+            double price = Double.parseDouble(request.getParameter("price"));
+            String description = request.getParameter("description");
+            String image = request.getParameter("image");
+
+            Product p = new Product();
+            p.setName(name);
+            p.setBrand(brand);
+            p.setPrice(price);
+            p.setDescription(description);
+            p.setImage(image);
+
+            dao.addProduct(p);
+        } else if ("delete".equals(action)) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            dao.deleteProduct(id);
+        }
+        response.sendRedirect("admin");
+    }
 }
